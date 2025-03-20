4a24a233fdd21d5d46320e01f140084c277955fc
Arlandaren <bad-07@list.ru>
2025-03-20 20:29:44 +0900

    chore: remove python server

--- src/serverp/api/routes/__init__.py
+++
@@ -1,7 +0,0 @@
-from fastapi import APIRouter
-from server.api.routes import login, qr, notifications
-
-api_router = APIRouter()
-api_router.include_router(login.router, tags=["login"])
-api_router.include_router(qr.router, tags=["qr"])
-api_router.include_router(notifications.router, tags=["notifications"])
