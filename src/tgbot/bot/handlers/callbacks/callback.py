7a50175f7c25e63cfb5effe08d4ef9fb2b524c25
hurper <103708209+BruSeLY@users.noreply.github.com>
2025-03-21 14:45:40 +0300

    .

--- src/tgbot/bot/handlers/callbacks/callback.py
+++ src/tgbot/bot/handlers/callbacks/callback.py
@@ -2,6 +2,9 @@ from aiogram.types import CallbackQuery
 from bot.core.config import TELEGRAM_CHANNEL
 from aiogram import Router, types
 from aiogram import F
+from ...repository.db.subscription import get_subscriptions_by_user_id
+import aiohttp
+
 r = Router()
 
 @r.callback_query(F.data == "check_subscription")
@@ -10,16 +13,8 @@ async def check_subscription(callback: CallbackQuery):
 
     chat_member = await callback.bot.get_chat_member(chat_id=TELEGRAM_CHANNEL, user_id=user_id)
 
-    # if chat_member.status in ["member", "administrator", "creator"]:
-    #     await callback.message.answer("Спасибо за подписку! ❤️\nИспользуйте промокод `telegram` и получите скидку 5% на бронирование.")
-    # else:
-    #     await callback.message.answer("Вы ещё не подписались на канал. Пожалуйста, подпишитесь и нажмите кнопку ещё раз.")
 
-
-import aiohttp
-
-
-QR_CODE_URL = "http://103.88.241.21/qr"  
+QR_CODE_URL = "http://103.88.241.21/qr"
 
 @r.callback_query(F.data == "get_qr")
 async def get_qr(callback: types.CallbackQuery):
