f3701a9ad7ad50a3d6d74a9ff6995a73a5074158
rngsurrounded <kircore970@gmail.com>
2025-03-21 19:15:17 +0900

    ok

--- src/server/migrations/4_alter_feedback.up.sql
+++ src/server/migrations/4_alter_feedback.up.sql
@@ -1,20 +1,22 @@
 ALTER TABLE feedback 
-  ADD COLUMN created_at_str VARCHAR(50),
-  ADD COLUMN answered_at_str VARCHAR(50);
+  ADD COLUMN created_at_text TEXT,
+  ADD COLUMN answered_at_text TEXT;
 
 UPDATE feedback 
 SET 
-  created_at_str = created_at::TEXT,
-  answered_at_str = answered_at::TEXT;
+  created_at_text = created_at::TEXT,
+  answered_at_text = answered_at::TEXT;
 
 ALTER TABLE feedback 
   DROP COLUMN created_at,
   DROP COLUMN answered_at;
 
 ALTER TABLE feedback 
-  RENAME COLUMN created_at_str TO created_at,
-  RENAME COLUMN answered_at_str TO answered_at;
+  RENAME COLUMN created_at_text TO created_at,
+  RENAME COLUMN answered_at_text TO answered_at;
 
 ALTER TABLE feedback 
-  ALTER COLUMN created_at SET NOT NULL,
+  ALTER COLUMN created_at SET NOT NULL;
+
+ALTER TABLE feedback 
   ALTER COLUMN created_at SET DEFAULT to_char(NOW(), 'YYYY-MM-DD"T"HH24:MI:SS"Z"');
