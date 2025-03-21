99d6179dbf8d865f2700e1bf806678dd9961cb62
rngsurrounded <kircore970@gmail.com>
2025-03-21 16:12:24 +0900

    funny sql

--- src/server/migrations/0_init.up.sql
+++ src/server/migrations/0_init.up.sql
@@ -1,7 +1,8 @@
+-- When target_id = 0, this is a broadcast notification to all users with the given org_token
 CREATE TABLE notifications (
                         id SERIAL PRIMARY KEY,
                         message TEXT NOT NULL,
-                        target_id BIGINT NOT NULL,
+                        target_id BIGINT NOT NULL, 
                         org_token TEXT NOT NULL,
                         created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
