e36d044a2e712a3036d9af379b98e560f879f9db
hurper <103708209+BruSeLY@users.noreply.github.com>
2025-03-21 18:11:27 +0300

    .

--- src/tgbot/bot/handlers/common/start.py
+++ src/tgbot/bot/handlers/common/start.py
@@ -22,32 +22,35 @@ async def handle_start_with_invite_code(
     params = invite_code.split("|")
 
     if len(params) != 2:
+        await message.answer("❌ Неверный формат кода приглашения.")
         return
-    id = params[0]
-    token = params[1]
 
-    await create_user(message.from_user.id, message.from_user.username)
-    await create_subscription(message.from_user.id, token, id)
+    patient_id, token = params
 
+    if not await user_exists_by_telegram_id(message.from_user.id):
+        await create_user(message.from_user.id, message.from_user.username)
+
+    await create_subscription(message.from_user.id, token, patient_id)
+    await message.answer("✅ Вы успешно подписались на уведомления.")
 
 @r.message(CommandStart())
 async def start_handler(
     message: Message, command: CommandObject, dialog_manager: DialogManager
 ) -> None:
     user_id = int(message.from_user.id)
-    invite_code = command.args
+    invite_code = command.args  
 
     if not await user_exists_by_telegram_id(user_id):
-        if invite_code:
-            await handle_start_with_invite_code(message, invite_code, dialog_manager)
         await create_user(user_id, message.from_user.username)
+
+    if invite_code:
+        await handle_start_with_invite_code(message, invite_code, dialog_manager)
 
     if not await have_user_access_by_telegram_id(user_id):
         await message.answer("*Упс...* Пока что у вас нет доступа к этому боту ⏳")
         return
 
     user = await get_user_by_telegram_id(user_id)
-
     if user["username"] != message.from_user.username:
         await update_username_by_telegram_id(user_id, message.from_user.username)
 
