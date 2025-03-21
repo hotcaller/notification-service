8954413e6aed457f665a065d0985ec5ba2bf7abe
rngsurrounded <kircore970@gmail.com>
2025-03-21 18:16:18 +0900

    WOW

--- src/server/internal/domains/subscriptions/models/subscription.go
+++ src/server/internal/domains/subscriptions/models/subscription.go
@@ -1,8 +1,8 @@
 package models
 
 type Subscription struct {
-	ID        int       `json:"id"`
-	TelegramID   string    `json:"telegram_id"`
-	Token  int64     `json:"token"`
-	PatientID  string    `json:"patient_id"`
+	ID         int    `json:"id" db:"id"`
+	TelegramID string `json:"telegram_id" db:"telegram_id"`
+	Token      int64  `json:"token" db:"token"`
+	PatientID  string `json:"patient_id" db:"patient_id"`
 }
